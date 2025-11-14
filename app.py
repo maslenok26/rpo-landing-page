1cef05768be02c826c4b834cd23e10b9de93201a
Fedor <test@test.com>
2025-11-14 15:09:14 +0300

    ы

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
 
 app.run(debug=True)
